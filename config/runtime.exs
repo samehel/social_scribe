bad01d534e9eeba22b73a5be3451d62d5664f1a8
samehel <sameh.elwakeel75@gmail.com>
2025-12-14 04:44:26 +0200

    Add origin check for production endpoint to prevent CSRF attacks

--- config/runtime.exs
+++ config/runtime.exs
@@ -84,6 +84,7 @@ if config_env() == :prod do
 
   config :social_scribe, SocialScribeWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
+    check_origin: ["https://" <> host],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
